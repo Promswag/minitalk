06bdddd35c95d20a761911598586801743355eca
Guillaume Baumgartner <gbaumgar@k0r2p13.42mulhouse.fr>
2022-09-30 17:46:03 +0200

    v1

--- src/client.c
+++ src/client.c
@@ -6,18 +6,65 @@
 /*   By: gbaumgar <gbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:21:50 by gbaumgar          #+#    #+#             */
-/*   Updated: 2022/09/28 17:20:58 by gbaumgar         ###   ########.fr       */
+/*   Updated: 2022/09/30 17:34:22 by gbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	terminate_message_to_pid(int pid, int bit)
+{
+	while (bit--)
+	{
+		kill(pid, SIGUSR2);
+		usleep(69);
+	}
+}
+
+void	send_message_to_pid(int pid, char *message)
+{
+	int	bit;
+	int	r;
+
+	while (*message)
+	{
+		bit = 8;
+		while (bit--)
+		{
+			if (((*message >> bit) % 2))
+				r = kill(pid, SIGUSR1);
+			else
+				r = kill(pid, SIGUSR2);
+			if (r)
+			{
+				ft_printf("Proccess with PID %d is unreachable.\n", pid);
+				exit(0);
+			}
+			usleep(69);
+		}
+		message++;
+	}
+	terminate_message_to_pid(pid, 8);
+}
+
+void	signal_ack(int signal)
+{
+	if (signal == SIGUSR1)
+	{
+		ft_printf("Message sent.\n");
+		exit(0);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
-		kill(ft_atoi(argv[1]), SIGUSR1);
+		signal(SIGUSR1, signal_ack);
+		send_message_to_pid(ft_atoi(argv[1]), argv[2]);
 		while (1)
 			pause();
 	}
 }
+
+
--- src/server.c
+++ src/server.c
@@ -6,19 +6,47 @@
 /*   By: gbaumgar <gbaumgar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 15:18:08 by gbaumgar          #+#    #+#             */
-/*   Updated: 2022/09/28 17:16:19 by gbaumgar         ###   ########.fr       */
+/*   Updated: 2022/09/30 17:36:28 by gbaumgar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	signal_handler(int signal, siginfo_t *siginfo, void *context)
+{
+	static int				i;
+	static unsigned char	c;
+
+	(void)context;
+	c <<= 1;
+	if (signal == SIGUSR1)
+		c += 1;
+	if (!i)
+		i = 0;
+	if (++i == 8)
+	{
+		i = 0;
+		if (c == 0)
+		{
+			ft_printf("\n");
+			kill(siginfo->si_pid, SIGUSR1);
+		}
+		else
+			ft_printf("%c", c);
+	}
+}
+
 int	main(void)
 {
-	pid_t		pid;
-	// sigset_t	sig;
+	pid_t				pid;
+	struct sigaction	sa;
 
 	pid = getpid();
 	ft_printf("%u\n", (int)pid);
+	sa.sa_flags = 0;
+	sa.sa_sigaction = signal_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
 		pause();
 }
